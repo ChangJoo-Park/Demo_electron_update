9407885a573662c688ef61837fcd56ca565de3e6
Vijayant Katyal <vijayantkatyal+github@outlook.com>
2016-12-06 22:24:11 +0530

    [Added] auto update option

--- app/renderer.js
+++ app/renderer.js
@@ -2,25 +2,29 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-var electron_auto_updater_1 = require('electron-auto-updater');
+const app = require('electron').remote.app; //require('electron-auto-updater');
+const updater = require('electron').remote.require('electron-auto-updater');
 
-electron_auto_updater_1.autoUpdater.checkForUpdates();
-electron_auto_updater_1.autoUpdater.addListener("update-available", function (event) {
-    log("A new update is available");
+console.log("current version: " + app.getVersion());
+
+updater.autoUpdater.checkForUpdates();
+updater.autoUpdater.addListener("update-available", function (event) {
+    console.log("A new update is available");
 });
-electron_auto_updater_1.autoUpdater.addListener("update-downloaded", function (event, releaseNotes, releaseName, releaseDate, updateURL) {
-    console.log("A new update is ready to install", "Version " + releaseName + " is downloaded and will be automatically installed on Quit");
-    console.log("quitAndInstall");
-    electron_auto_updater_1.autoUpdater.quitAndInstall();
-    return true;
-});
-electron_auto_updater_1.autoUpdater.addListener("error", function (error) {
-    console.log(error);
-});
-electron_auto_updater_1.autoUpdater.addListener("checking-for-update", function (event) {
-    console.log("checking-for-update");
-});
-electron_auto_updater_1.autoUpdater.addListener("update-not-available", function () {
+
+updater.autoUpdater.addListener("update-not-available", function () {
     console.log("update-not-available");
 });
 
+updater.autoUpdater.addListener("update-downloaded", (event, releaseNotes, releaseName, releaseDate, updateURL) => {
+    console.log("A new update is ready to install", `Version ${releaseName} is downloaded and will be automatically installed on Quit`);
+    console.log("quitAndInstall");
+    updater.autoUpdater.quitAndInstall();
+    return true;
+})
+updater.autoUpdater.addListener("error", (error) => {
+    console.log(error);
+});
+updater.autoUpdater.addListener("checking-for-update", (event) => {
+    console.log("checking-for-update");
+});
